ff475cba248c127deaeb5f987aff3adf835a9470
Arshiad09 <102972093+Arshiad09@users.noreply.github.com>
2024-08-04 19:22:13 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import './index.css';
+import Header from "./components/common/header";
+import Footer from "./components/common/footer";
+function App() {
+  return (
+    <div className="App">
+      <Header />
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
